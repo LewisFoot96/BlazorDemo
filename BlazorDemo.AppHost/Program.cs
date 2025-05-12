8f64af8463586f0dc2afc7cf84d29a3565d989a4
Lewis Foot <lfoot91@hotmail.co.uk>
2025-05-12 16:51:28 +0100

    feat: added wpf and windows forms app

--- BlazorDemo.AppHost/Program.cs
+++ BlazorDemo.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.BlazorDemo_Hyrbid_Web>("blazordemo-hyrbid-web");
 
+builder.AddProject<Projects.BlazorDemo_Hybrid_WindowsForms>("blazordemo-hybrid-windowsforms");
+
 builder.Build().Run();
