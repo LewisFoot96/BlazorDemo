9e9243c0eb3de3279bc5446276ca321a7c2ddabd
Lewis Foot <lfoot91@hotmail.co.uk>
2025-05-14 18:49:23 +0100

    fix: correct win forms setup

--- BlazorDemo.Hybrid.WindowsForms/Form1.cs
+++ BlazorDemo.Hybrid.WindowsForms/Form1.cs
@@ -1,10 +1,17 @@
+using BlazorDemo.Hyrbid.Shared;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace BlazorDemo.Hybrid.WindowsForms
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1(ServiceProvider serviceProvider)
         {
             InitializeComponent();
+            blazorWebView1.HostPage = "wwwroot/index.html";
+            blazorWebView1.Services = serviceProvider;
+            blazorWebView1.RootComponents.Add(new RootComponent("#app", typeof(Routes), null));
         }
     }
 }
